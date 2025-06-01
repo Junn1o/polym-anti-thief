942032f725a56ce352355e053895480e2e3776bc
Junnio <30598484+Junn1o@users.noreply.github.com>
2025-06-02 01:43:12 +0700

    system log hopper place under shulker box, minecart with hopper block trasfer item

--- src/client/java/com/junnio/PolymantithiefClient.java
+++ src/client/java/com/junnio/PolymantithiefClient.java
@@ -2,12 +2,39 @@ package com.junnio;
 
 import com.junnio.net.ModNetworking;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.ShulkerBoxBlockEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 
 public class PolymantithiefClient implements ClientModInitializer {
 	public static final String MOD_ID = "polym-anti-thief";
 
 	@Override
 	public void onInitializeClient() {
+		UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
+			if (world.isClient) return ActionResult.PASS;
+
+			// Only care about placing hoppers
+			if (!player.getStackInHand(hand).isOf(Blocks.HOPPER.asItem())) return ActionResult.PASS;
+
+			BlockPos clickedPos = hitResult.getBlockPos();
+			Direction side = hitResult.getSide();
+			BlockPos placePos = clickedPos.offset(side); // Hopper will be placed here
+
+			// Check if there's a Shulker Box above the place position
+			BlockPos above = placePos.up();
+			BlockEntity beAbove = world.getBlockEntity(above);
+			if (beAbove instanceof ShulkerBoxBlockEntity) {
+				player.sendMessage(Text.of("You are placing a hopper under a shulker box!"), false);
+			}
+
+			return ActionResult.PASS;
+		});
 		ModNetworking.init();
 	}
 
--- src/main/java/com/junnio/mixin/ExampleMixin.java
+++ src/main/java/com/junnio/mixin/ExampleMixin.java
@@ -1,15 +1,35 @@
 package com.junnio.mixin;
 
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.ShulkerBoxBlockEntity;
+import net.minecraft.entity.vehicle.HopperMinecartEntity;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(MinecraftServer.class)
+@Mixin(HopperMinecartEntity.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
+	@Inject(method = "canOperate", at = @At("HEAD"), cancellable = true)
+	private void blockShulkerBoxPull(CallbackInfoReturnable<Boolean> cir) {
+		HopperMinecartEntity self = (HopperMinecartEntity) (Object) this;
+
+		BlockPos above = self.getBlockPos().up();
+		BlockEntity be = self.getWorld().getBlockEntity(above);
+
+		if (be instanceof ShulkerBoxBlockEntity shulker) {
+			// Check if the shulker box has a custom name
+			if (shulker.hasCustomName()) {
+				String customName = shulker.getCustomName().getString();
+
+				if ("NoPull".equalsIgnoreCase(customName)) {
+					// Prevent item pulling only if name matches "NoPull"
+					cir.setReturnValue(false);
+				}
+			}
+		}
 	}
 }
